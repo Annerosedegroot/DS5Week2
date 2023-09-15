91ff4acff778bbaff85756f0161a2253cfce8781
EllaCoolen <508779@student.fontys.nl>
2023-09-15 10:38:47 +0200

    Bij mij doet hij het wel

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def main(file_path):
     average = functie2.functie2(records)
     return average
 
-file_path = "D:\Users\Marilene\Documents\DS5 week2\DS5Week2\squirrel_edges.csv"
+file_path = "C:\Users\Ella\Documents\TOEGEPASTE WISKUNDE\Leerjaar 2\DS5\Week 2\DS5Week2\squirrel_edges.csv"
 print(main(file_path))
