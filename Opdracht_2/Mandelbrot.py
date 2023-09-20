8b3998610598bf47a67dcb70e6b757a824123f8c
EllaCoolen <508779@student.fontys.nl>
2023-09-20 15:47:27 +0200

    Tijdens onze meeting

--- Opdracht_2/Mandelbrot.py
+++ Opdracht_2/Mandelbrot.py
@@ -1,13 +1,68 @@
 # importeer alle nodige libraries
 
-import math
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import cmath
-# 
 
+def calculate_mandelbrot(c: complex, max_iter: int) -> int:
+    """
+    Calculate the diverging index for a given complex number c in the Mandelbrot set.
+    
+    Args:
+        c (complex): The complex number for which we want to calculate the diverging index.
+        max_iter (int): The maximum number of iterations to check for divergence.
+        
+    Returns:
+        int: The diverging index (or 0 if it does not diverge).
+    """
+    z = 0
+    for n in range(max_iter):
+        if abs(z) > 2:
+            return n
+        z = z**2 + c
+    return 0
 
-x = np.arange(-1.5, 0.5, 0.1)
-for i in x:
-    print(i)
+def generate_mandelbrot_image(width: int, height: int, x_range: tuple, y_range: tuple, max_iter: int) -> np.ndarray:
+    """
+    Generate a Mandelbrot image.
+
+    Args:
+        width (int): Width of the image.
+        height (int): Height of the image.
+        x_range (tuple): Tuple containing the minimum and maximum x values for the range.
+        y_range (tuple): Tuple containing the minimum and maximum y values for the range.
+        max_iter (int): The maximum number of iterations to check for divergence.
+
+    Returns:
+        np.ndarray: A numpy array representing the Mandelbrot image.
+    """
+    x_min, x_max = x_range
+    y_min, y_max = y_range
+    image = np.zeros((height, width))
+
+    for i in range(height):
+        for j in range(width):
+            x = x_min + (x_max - x_min) * j / (width - 1)
+            y = y_min + (y_max - y_min) * i / (height - 1)
+            c = complex(x, y)
+            image[i, j] = calculate_mandelbrot(c, max_iter)
+
+    return image
+
+def plot_mandelbrot_image(image: np.ndarray):
+    """
+    Visualize the Mandelbrot image using matplotlib.
+
+    Args:
+        image (np.ndarray): A numpy array representing the Mandelbrot image.
+    """
+    plt.imshow(image, cmap='hot', extent=[-1.5, 0.5, -1, 1])
+    plt.colorbar()
+    plt.title('Mandelbrot Set')
+    plt.xlabel('Real')
+    plt.ylabel('Imaginary')
+    plt.show()
+
+# Example usage:
+if __name__ == '__main__':
+    mandelbrot_image = generate_mandelbrot_image(200, 200, (-1.5, 0.5), (-1, 1), max_iter=100)
+    plot_mandelbrot_image(mandelbrot_image)
