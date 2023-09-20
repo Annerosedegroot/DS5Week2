6bd4acc3815b73c08f3c3abc330e702a531f012b
Annerosedegroot <433534@student.fontys.nl>
2023-09-20 15:44:43 +0200

    dingen

--- Opdracht_2/Mandelbrot.py
+++ Opdracht_2/Mandelbrot.py
@@ -1,1 +1,13 @@
-# Mandelbrot
+# importeer alle nodige libraries
+
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath
+# 
+
+
+x = np.arange(-1.5, 0.5, 0.1)
+for i in x:
+    print(i)
