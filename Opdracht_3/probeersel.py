063392d05f243d54c34b74fe6ba5910af3f5f9b0
marilene805 <517774@student.fontys.nl>
2023-09-21 21:19:17 +0200

    gebrobeerd 3.1, werkt bijna maar net niet juist

---
+++ Opdracht_3/probeersel.py
@@ -0,0 +1,71 @@
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
+
+#niet schrikken dat het netwerk maar 10 nodes heeft, kun je aanpassen in regel 26 maar doe maar pas als alles werkt
+
+def genereer_barabasi(n0, n, M):
+    """Genereert een netwerk gebaseerd op het barabasi-albert algoritme
+    Input:
+    ----------
+    no: int
+        startaantal nodes aan start_graph
+    n: int
+        Aantal gewenste totale nodes
+    M: int
+        aantal edges vanuit iedere nieuwe node
+    
+    Return
+    -----------
+    G: graph (ontstane barabasi netwerk)
+    """
+    # Maak een ster netwerk met starthoeveelheid nodes n0
+    G = nx.star_graph(n0)
+
+    # Voeg de resterende nodes een voor een toe
+    for i in range(n0 + 1, 10): ############10 moet uiteindelijk veranderen naar n+1, 
+                                #maar dit is overzichtelijker voor wat er gebeurd. Check ook de probability dit is raar
+                                #node 0 zou toch de kleinste kans moeten hebben om verbonden te geraken met node 6, die lijst zou omgekeer dmoeten zijn???
+        # Voeg de nieuwe node toe
+        G.add_node(i)
+
+        # Lijst van alle bestaande nodes
+        existing_nodes = list(G.nodes)
+
+        # Bereken de kansverdeling (page rank?) voor het verbinden van de nieuwe node met bestaande nodes
+        node_degrees = dict(G.degree())
+        prob = [node_degrees[node] / sum(node_degrees.values()) for node in G.nodes]
+        print(i, prob)
+        # Verbind de nieuwe node met M bestaande nodes op basis van de kansverdeling
+        selected_nodes = random.choices(existing_nodes, weights=prob, k=M)
+
+        # Voeg edges toe zonder zelfverbindingen
+        selected_nodes = [node for node in selected_nodes if node != i]
+
+        for node in selected_nodes:
+            G.add_edge(i, node)
+    
+    return G
+
+def plot_network(G):
+    # Genereer de layout met spring_layout
+    pos = nx.spring_layout(G)
+
+    # Teken het netwerk
+    nx.draw(G, pos, with_labels=True, node_size=50)
+    plt.show()
+
+# Aantal initiële nodes in de ster
+n0 = 5
+
+# Het gewenste totale aantal nodes in het netwerk
+n = 400
+
+# Aantal nodes waarmee elke nieuwe node zich verbindt
+M = 4
+
+# Creëer het netwerk
+G = genereer_barabasi(n0, n, M)
+
+# Teken het netwerk
+plot_network(G)
