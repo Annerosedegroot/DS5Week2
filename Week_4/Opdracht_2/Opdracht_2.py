66aa82d54e37bd2cf336e4be36f137c8a7a459b3
EllaCoolen <508779@student.fontys.nl>
2023-10-06 11:55:17 +0200

    Meer cleanen

--- Week_4/Opdracht_2/Opdracht_2.py
+++ Week_4/Opdracht_2/Opdracht_2.py
@@ -6,15 +6,18 @@ df = pd.read_excel('hotelBookings.xlsx')
 # in kolom stays_in_week_nights is een 4.3
 df['stays_in_weekend_nights'] = df['stays_in_weekend_nights'].replace([4.3], 4)
 
-    
 
 # kolom lead_time is een outlier 737, de rest is tussen 0 en 400
+
+
 
 # country heeft een 2 en 3 en NULL
 # if df['country'] == '2' or '3' or 'NULL':
 #     df.drop()
 
 # country lay out sommige spaties
+df['country'] = df['country'].str.replace(' ', '')
+
 
 # arrival_date_year is een jaar 2099
 
